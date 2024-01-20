ea01c2ebad8784c364892a25ecc4e1ef7978be7e
Powell <powell.payofilin2400@gmail.com>
2024-01-20 17:24:57 +0800

    added dialogue boxes depending on the object interacted with, edited onCollision for this

--- lib/components/interactable_block.dart
+++ lib/components/interactable_block.dart
@@ -1,5 +1,6 @@
 import 'package:flame/components.dart';
 import 'package:flame/collisions.dart';
+import 'package:rescue_odyssey/components/dialogue_box.dart';
 
 import 'package:rescue_odyssey/entities/player.dart';
 
@@ -13,7 +14,9 @@ import 'package:rescue_odyssey/game/rescue_odyssey_game.dart';
 class InteractableBlock extends PositionComponent with CollisionCallbacks, HasGameReference<RescueOdysseyGame>  {
   /// The hitbox used for collision detection in maps.
   late final RectangleHitbox hitBox;
-
+  /// Checks if dialogue is on screen or not
+  bool dialogueIsNotOnScreen = true;
+  /// The dialogue property to be read as a node in object dialogues yarn file
   String interactableDialogue;
 
   InteractableBlock({required position, required size, required this.interactableDialogue}) : super(position: position, size: size);
@@ -29,9 +32,23 @@ class InteractableBlock extends PositionComponent with CollisionCallbacks, HasGa
 
   @override
   void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    DialogueBox dialogueBox = DialogueBox(dialogueProperty: interactableDialogue);
     super.onCollision(intersectionPoints, other);
-    if (other is Player && intersectionPoints.length == 2) {
-
+    if(dialogueIsNotOnScreen) {
+      if (other is Player && intersectionPoints.length == 2) {
+        game.camera.viewport.add(dialogueBox);
+        dialogueIsNotOnScreen = false;
+      } else {
+        if(dialogueBox.dialogueBuilder.isDialogueFinished){
+          game.camera.viewport.remove(dialogueBox);
+        }
+      }
     }
+  }
+
+  @override
+  void onCollisionEnd(PositionComponent other) {
+    dialogueIsNotOnScreen = true;
+    super.onCollisionEnd(other);
   }
 }
